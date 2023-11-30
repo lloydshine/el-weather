c9a36b9d7bc2ab97356e153041b5818a35e419b6
Lloyd Semblante <92197616+lloydshine@users.noreply.github.com>
2023-11-30 02:32:41 +0000

    done

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -10,19 +10,19 @@ export default function Details({ weather }) {
   return (
     <section className="details">
       <div className="info">
-        <WiNightCloudyWindy size={80} />
+        <WiNightCloudyWindy size={100} />
         <p>Wind Speed: {weather.wind_speed_10m}</p>
       </div>
       <div className="info">
-        <WiNightRainWind size={80} />
+        <WiNightRainWind size={100} />
         <p>Precipitation: {weather.precipitation}</p>
       </div>
       <div className="info">
-        <WiHumidity size={80} />
+        <WiHumidity size={100} />
         <p>Humidity: {weather.relative_humidity_2m}</p>
       </div>
       <div className="info">
-        <WiEarthquake size={80} />
+        <WiEarthquake size={100} />
         <p>Surface Pressure: {weather.surface_pressure}</p>
       </div>
     </section>
